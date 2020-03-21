4f1fccf7b44236feb87d3e520696a1feae2980ce
Francesco Sacchi <batt@develer.com>
2020-03-21 12:23:52 +0100

    Add proper pin names; add RS485 pins; enable modbus.

--- battshutter.ino
+++ battshutter.ino
@@ -1,35 +1,41 @@
-//#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
-//#include <ArduinoModbus.h>
+#include <ArduinoRS485.h> // ArduinoModbus depends on the ArduinoRS485 library
+#include <ArduinoModbus.h>
 #include "motor.h"
 
 const int ledPin = LED_BUILTIN;
 
-#define UP_SWITCH   16 // A2
-#define DOWN_SWITCH 15 // A1
+#define UP_SWITCH   A2
+#define DOWN_SWITCH A1
 
+#define DE_RS485 A0
+#define RE_RS485 A3
+
+#define MODBUS_ON 1
 
 void setup() {
+  RS485.setPins(RS485_DEFAULT_TX_PIN, DE_RS485, RE_RS485);
+  
+#if !MODBUS_ON
   Serial.begin(9600);
-
   Serial.println("BattShutter start");
+#endif
 
   encoder_init();
   motor_init();
   pinMode(UP_SWITCH, INPUT_PULLUP);
   pinMode(DOWN_SWITCH, INPUT_PULLUP);
 
-  #if 0
+#if MODBUS_ON
   // start the Modbus RTU server, with (slave) id 1
   if (!ModbusRTUServer.begin(1, 9600)) {
     Serial.println("Failed to start Modbus RTU Server!");
     while (1);
   }
-  #endif
 
-  #if 0
   // configure a single coil at address 0x00
   ModbusRTUServer.configureCoils(0x00, 1);
-  #endif
+  pinMode(ledPin, OUTPUT);
+#endif
 }
 
 void loop() {
@@ -40,7 +46,8 @@ void loop() {
     motor_poll(MS_DOWN);
   else
     motor_poll(MS_STOP);
-  #if 0
+    
+#if MODBUS_ON
   // poll for Modbus RTU requests
   ModbusRTUServer.poll();
 
@@ -55,5 +62,5 @@ void loop() {
     // coild value clear, turn LED off
     digitalWrite(ledPin, LOW);
   }
-  #endif
+#endif
 }
